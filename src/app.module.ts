546bbfd514001d85454bef0d5a3579dd37642e80
lshinjikashvili <lshinjikashvili@crocobet.com>
2024-06-25 12:11:56 +0400

    feat:aa

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import {MongooseModule} from "@nestjs/mongoose";
+const uri = "mongodb+srv://shinjikashvililuka22:shinjikashvililuka22@cluster0.smntvqn.mongodb.net/?appName=Cluster0";
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule,MongooseModule.forRoot(uri)],
   controllers: [AppController],
   providers: [AppService],
 })
